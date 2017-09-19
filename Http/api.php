8b10012e3094ac428302f1edaf8d09848477c805
diuliano <diuliano0@gmail.com>
2017-09-19 10:49:20 -0300

    fix

--- Http/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-Route::group(['prefix'=>'v1','middleware' => ['cors']], function () {
-    \Modules\Core\Rotas\UserRoute::run();
-    \Modules\Core\Rotas\RoleRoute::run();
-    \Modules\Core\Rotas\PermissionRoute::run();
-    \Modules\Core\Rotas\RotaAcessoRoute::run();
-    \Modules\Core\Rotas\NewsletterRoute::run();
-});
